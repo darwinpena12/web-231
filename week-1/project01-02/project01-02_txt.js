faad14fbf63e796c3e6671098837deb19962ecf4
Darwin <darwinpena12@gmail.com>
2026-01-20 13:08:50 -0500

    Initial commit.

---
+++ week-1/project01-02/project01-02_txt.js
@@ -0,0 +1,13 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: darwin Pena Cabrera
+    Date: 01/20/2026
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+let service1Name = "Basic", service2Name ="Express", service3Name = "Extreme", service4Name = "Ultimate", service1Speed = "50 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig"
